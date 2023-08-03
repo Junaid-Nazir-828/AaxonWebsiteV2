83d5fd902a344748cfa554dbb967d32b27902abf
juniad-hub <junaidnazir7501@gmail.com>
2023-08-03 16:47:37 +0500

    minor change

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         nunito:["Poppins","Roboto"],
       },
       backgroundImage: {
-        'map': "url('../public/map.png')",
+        'map': "url('/mapImg.png')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
